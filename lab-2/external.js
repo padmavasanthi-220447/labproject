db1d2dd33be68bced55d02be4e4c420b6bab41fc
padmavasanthi-220447 <n220447@rguktn.ac.in>
2026-01-06 16:24:32 +0530

    add file

---
+++ lab-2/external.js
@@ -0,0 +1,8 @@
+let cropName="Rice"
+let croptype="Seasonal"
+let soilmoisture=30
+let growthstage="vagetative"
+let sowingDate="2026-01-10"
+let harvestDate="2025-05-18"
+const farmername="Ramanjaneyulu"
+console.log(cropName)
